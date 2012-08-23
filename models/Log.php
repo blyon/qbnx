82bd693ae78e758c4f3212c105d3d174692791d5
Brandon Lyon <brandon.lyon@business.com>
2012-08-22 22:36:24 -0700

    removed colons from logfile name

--- models/Log.php
+++ models/Log.php
@@ -51,7 +51,7 @@ class Log
             $this->directory .= "/";
         }
 
-        $file = $this->directory . date('Y-m-d_H:i:s') . ".log";
+        $file = $this->directory . date('Y-m-d_His') . ".log";
         if (FALSE === ($this->_fh = fopen($file, "a"))) {
             throw new Exception("Failed to create log file: " . $file);
         }
